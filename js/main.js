5021823bdfbfa25bc37652ffef656a292e2d35b3
shui91 <sherman.sy.hui@gmail.com>
2015-10-23 00:07:02 -0700

    feat: added validation and default value in Model

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var Song = Backbone.Model.extend({
+	defaults: {
+		genre: "Rock"
+	},
+
+	validate: function(attrs){
+		if (!attrs.title){
+			return "Title is required!";
+		}
+	},
+
 	initialize: function(){
 		console.log("New Song has been created!")
 	}
 });
 
-var song = new Song();
+var song = new Song({
+	title: "Back In Black",
+	artist: "ACDC",
+	publishYear: "1980"
+});
