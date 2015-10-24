a51c4b3601dac2a94cc7072c2f4921e71c7529dc
shui91 <sherman.sy.hui@gmail.com>
2015-10-23 21:21:54 -0700

    feat: playing with Collections of Cars, making filters and removing cars

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var Vehicle = Backbone.Model.extend({
 
 	validate: function(attrs){
 		if (!attrs.registrationNumber){
-			return "Registration Number is required."
+			return "Registration Number is required.";
 		}
 	},
 
@@ -19,55 +19,51 @@ var Vehicle = Backbone.Model.extend({
 	}
 });
 
+
 var Car = Vehicle.extend({
 	start: function(){
-		console.log("Car with registration number " + this.get("registrationNumber") + " and color " + this.get("color") + " started.")
+		console.log("Car with registration number " + this.get("registrationNumber") + " and color " + this.get("color") + " started.");
 	}
 });
 
-var car = new Car({
-	registrationNumber: "XLI887",
-	color: "Blue"
+var Cars = Backbone.Collection.extend({
+	model: Car
 });
 
-car.start();
+var cars = new Cars([
+	new Car({
+		registrationNumber: "XLI887",
+		colour: "Blue"
+	}),
+	new Car({
+		registrationNumber: "ZNP123",
+		colour: "Blue"
+	}),
+	new Car({
+		registrationNumber: "XUV456",
+		colour: "Gray"
+	})
+]);
 
-car.unset("registrationNumber");
+var matchedCar = cars.where({registrationNumber: "XLI887"});
+console.log("XLI887 Car", matchedCar);
+cars.remove(matchedCar);
 
-if (!car.isValid()){
-	console.log(car.validationError);
-}
+var blueCars = cars.filter(function(car){
+	return car.get("colour") == "Blue";
+});
 
-if (car.isValid()){
-	console.log("Car is Valid!")
-} else {
-	console.log(car.validationError);
-}
+console.log("Blue Cars", blueCars);
+
+console.log("Collection of Cars: ", cars.toJSON());
+
+cars.each(function(car){
+	console.log(car);
+})
 
 
-// car.fetch({
-// 	success: function(){
-
-// 	},
-// 	error: function(){
-
-// 	}
-// });
-
-// car.save({}, {
-// 	success: function(){
-
-// 	},
-// 	error: function(){
-
-// 	}
-// });
-
-// car.destroy({
-// 	success: function(){
-
-// 	},
-// 	error: function(){
-
-// 	}
-// })
+// Find all the Blue colours and log them in the console.
+// Find the car with the registration number XLI887 and log it in the console.
+// Remove this car from the collection.
+// Convert the collection to a JSON object and log it in the console.
+// Iterate the collection and log each car in the console.
